a971906d87e483d7b7f8c46af75afef70f43130d
Shoggato <eeels1986@gmail.com>
2023-09-03 16:07:16 -0500

    added sql schema, and also finished the crowdfunding_db for postgresql

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,43 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE subcategory(
+	subcategory_id VARCHAR NOT NULL,
+	subcategory VARCHAR NOT NULL,
+	PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE category(
+	category_id VARCHAR NOT NULL,
+	category VARCHAR NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+CREATE TABLE contacts(
+	contact_id INT NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	email VARCHAR NOT NULL,
+	PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE campaign (
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR NOT NULL,
+	description VARCHAR,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
+	outcome VARCHAR,
+	backers_count INT NOT NULL,
+	country VARCHAR,
+	currency VARCHAR,
+	launched_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR NOT NULL,
+	subcategory_id VARCHAR NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
